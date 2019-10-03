a96c6d308fe2b4f7f2416746526804d4a6dd1be4
Lucille Njoo <lucillenjoo@gmail.com>
2019-10-03 03:53:52 -0700

    Modify App.js and do some testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React, { useEffect } from 'react'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 import './App.css'
 
 import AppHeader from './AppHeader'
 import SearchForm from './SearchForm'
 
+import Homepage from './pages/Homepage'
+
 import { apiHost } from './api'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#c23063', // magenta
+      main: '#180142' // dark purple
+    },
+    type: 'dark',
+    dark: {
+      pink: '#c23063',
+      purple: '#180142',
+    },
+    medium: {
+      purple: '#5a0b66',
+    },
+    light: {
+      pink: '#db91aa',
+      purple: '#ac91db'
+    },
+    white: '#FFFFFF',
+  },
+  typography: {
+    fontFamily: ['Dosis', 'Roboto', 'Open Sans', 'IBM Plex Sans', '"Helvetica Neue"', 'Arial', 'sans-serif'].join(',')
+  }
+})
 
 const App = () => {
   // Because App is the "uppermost" component (see index.js), code in the useEffect function
@@ -18,8 +46,13 @@ const App = () => {
   // content (i.e., what the render() method returns for class-based components).
   return (
     <div className="App">
-      <AppHeader />
-      <SearchForm />
+      <MuiThemeProvider theme={theme}>
+        {/*
+        <AppHeader />
+        <SearchForm />
+        */}
+        <Homepage />
+      </MuiThemeProvider>
     </div>
   )
 }
