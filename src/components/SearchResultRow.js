29801e8e5eee088d53c0f892336f66c1a1a3fc43
Lucille Njoo <lucillenjoo@gmail.com>
2019-10-14 00:58:09 -0700

    Create React component for individual search result rows

---
+++ src/components/SearchResultRow.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = theme => ({
+  row: {
+    background: theme.palette.light.pink,
+    padding: 4,
+    margin: 1
+  },
+  text: {
+    color: theme.palette.dark.purple,
+  }
+})
+
+const SearchResultRow = props => {
+
+  const { classes, songName, songID, artistName, onTrackSelect } = props
+
+  return (
+    <Grid item xs={12} id={songID} className={classes.row} onClick={onTrackSelect}>
+      <Typography id={songID} className={classes.text}>
+        "{songName}," Artist: {artistName}
+      </Typography>
+    </Grid>
+  )
+}
+
+export default withStyles(styles)(SearchResultRow)
