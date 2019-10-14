a5bafa324be4c24ee843def2d32e7c33d21416f0
Lucille Njoo <lucillenjoo@gmail.com>
2019-10-14 00:59:42 -0700

    Create Search page based on sample Giphy SearchForm and add styling

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,43 +1,98 @@
 import React, { useState } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
 
-import '../SearchForm.css'
+// import '../SearchForm.css'
 
 import SearchResults from '../SearchResults'
 
-import { searchGifs } from '../api'
+import { searchSongs } from '../api'
 
-const SearchForm = () => {
+const styles = theme => ({
+  title: {
+    color: theme.palette.light.pink,
+    padding: 5,
+    fontSize: '3rem',
+    textAlign: 'center'
+  },
+  searchForm: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  searchField: {
+    width: '50%',
+    height: '1.5rem',
+    marginBottom: 10
+  },
+  buttonEnabled: {
+    color: theme.palette.white,
+    fontSize: '1rem',
+    background: theme.palette.dark.pink,
+    height: '50px',
+    width: '150px',
+    borderRadius: '30px',
+    border: 'none',
+    margin: 5
+  },
+  buttonDisabled: {
+    color: theme.palette.white,
+    fontSize: '1rem',
+    background: theme.palette.dark.pink,
+    height: '50px',
+    width: '150px',
+    borderRadius: '30px',
+    border: 'none',
+    margin: 5,
+    opacity: '0.3'
+  }
+})
+
+const SearchForm = (props) => {
+
+  const { classes } = props
+
   const [error, setError] = useState(null)
   const [query, setQuery] = useState('')
-  const [images, setImages] = useState([])
+  const [songs, setSongs] = useState([])
 
   const handleQueryChange = event => setQuery(event.target.value)
 
   const performQuery = async event => {
-    event.preventDefault()
+    event.preventDefault() // why?
 
-    setError(null)
+    setError(null) // why?
 
     try {
-      const result = await searchGifs({
-        rating: 'pg-13',
+      const result = await searchSongs({
+        // rating: 'pg-13',
         q: query
       })
 
-      setImages(result.data)
+      setSongs(result.tracks.items)
     } catch (error) {
       setError('Sorry, but something went wrong.')
     }
   }
 
-  return (
-    <form className="SearchForm" onSubmit={performQuery}>
-      <p>Enter a search term:</p>
+  const handleTrackSelect = (event) => {
+    // do api stuff
+    console.log(`CALLED HANDLE TRACK SELECT: selected track id ${event.target.id}`)
+  }
 
-      <input name="query" type="text" value={query} onChange={handleQueryChange} />
+  return (
+    <form className={classes.searchForm} onSubmit={performQuery}>
+      <Typography className={classes.title}>
+        search
+      </Typography>
+
+      <input name="query" type="text" value={query} onChange={handleQueryChange} className={classes.searchField}/>
 
       <div className="ButtonBar">
-        <button type="submit" disabled={!query}>Search Giphy!</button>
+        <button type="submit" disabled={!query} className={query ? classes.buttonEnabled : classes.buttonDisabled}>
+          search tracks
+        </button>
       </div>
 
       {error && (
@@ -46,9 +101,12 @@ const SearchForm = () => {
         </div>
       )}
 
-      <SearchResults results={images} />
+      <Grid container xs={10}>
+        <SearchResults results={songs} onTrackSelect={handleTrackSelect}/>
+      </Grid>
+
     </form>
   )
 }
 
-export default SearchForm
+export default withStyles(styles)(SearchForm)
