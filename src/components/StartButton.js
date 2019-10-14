7a3cd99ac41fbbfd2a4bc7c17ba294719c1648d8
Lucille Njoo <lucillenjoo@gmail.com>
2019-10-13 17:54:54 -0700

    Add href to search page, attempts at styling

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -2,25 +2,32 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import Fab from '@material-ui/core/Fab'
+import Typography from '@material-ui/core/Typography'
 
 const styles = theme => ({
   fab: {
     background: theme.palette.dark.purple,
     color: 'white',
-    height: 60,
-    width: 200,
+    height: 50,
+    width: 150,
     borderRadius: 30,
     margin: '0 auto',
-    fontSize: '1.125rem',
-    textAlign: 'center'
+    fontSize: '1.5rem',
+    textAlign: 'center',
+    testTransform: 'none'
+  },
+  text: {
+    testTransform: 'none'
   }
 })
 
 const StartButton = props => {
   const { classes } = props
   return (
-    <Fab variant="extended" color="primary" aria-label="Start" className={classes.fab}> {/*href="/search"*/} 
-      Start
+    <Fab variant="extended" color="primary" aria-label="Start" href="/search" className={classes.fab}> {/*href="/search"*/}
+      <Typography className={classes.text}>
+        start
+      </Typography>
     </Fab>
   )
 }
