ebb57772fc4dcdf520f45c9d68d8946af54e8ae6
Lucille Njoo <lucillenjoo@gmail.com>
2019-10-14 00:57:46 -0700

    Create song search results

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,13 +1,37 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
 
-import './SearchResults.css'
+import SearchResultRow from './components/SearchResultRow'
 
-import GiphyImage from './GiphyImage'
+const styles = theme => ({
+  searchResults: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginTop: '1rem'
+  }
+})
 
-const SearchResults = props => (
-  <div className="SearchResults">
-    {props.results.map(image => <GiphyImage key={image.id} image={image} />)}
-  </div>
-)
+const SearchResults = props => {
 
-export default SearchResults
+  const { classes, results, onTrackSelect } = props
+
+  return (
+    <div className={classes.searchResults}>
+      <Grid container>
+        {results.map(song => (
+          <SearchResultRow
+            key={song.id}
+            songName={song.name}
+            songID={song.id}
+            artistName={song.artists[0].name}
+            onTrackSelect={onTrackSelect}
+          />
+        ))}
+      </Grid>
+    </div>
+  )
+}
+
+export default withStyles(styles)(SearchResults)
