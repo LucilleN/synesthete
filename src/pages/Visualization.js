d1a4ab3e66de8691d9c15e19fdc67f9cd8235c77
Lucille Njoo <lucillenjoo@gmail.com>
2019-10-14 01:00:34 -0700

    Make basic div in Visualization so that it looks like a React component (to get rid of errors)

---
+++ src/pages/Visualization.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Visualization = props => {
+  // const { image } = props
+  return (
+    <div>
+      Visualization Page
+    </div>
+  )
+}
+
+export default Visualization
