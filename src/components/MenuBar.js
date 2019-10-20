b77430a9345b8fef08c129b6aa879ebaf1e0b22f
Lucille Njoo <lucillenjoo@gmail.com>
2019-10-20 11:18:55 -0700

    Fix bug in MenuBar and implement redirects to all pages

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -42,56 +42,81 @@ const styles = theme => ({
 
 const MenuBar = props => {
   const [homeSelected, setHomeSelected] = useState(false)
-
+  const [searchSelected, setSearchSelected] = useState(false)
+  const [uploadSelected, setUploadSelected] = useState(false)
+  const [aboutSelected, setAboutSelected] = useState(false)
 
   const handleHomeSelect = event => {
-    console.log("handleHomeSelect called")
-    event.preventDefault()
+    // console.log("handleHomeSelect called")
+    // event.preventDefault()
     setHomeSelected(true)
-    console.log(`homeSelected is ${homeSelected}`)
+    // console.log(`homeSelected is ${homeSelected}`)
   }
+
+  const handleSearchSelect = event => {
+    setSearchSelected(true)
+  }
+
+  const handleUploadSelect = event => {
+    setUploadSelected(true)
+  }
+
+  const handleAboutSelect = event => {
+    setAboutSelected(true)
+  }
+
+  useEffect(() => {
+    setHomeSelected(false)
+    setSearchSelected(false)
+    setUploadSelected(false)
+    setAboutSelected(false)
+  })
 
   const { classes } = props
 
-  if (homeSelected) {
-    // setHomeSelected(false)
-    return (
-      <Redirect to={'/'} />
-    )
-  }
-
-  // useEffect(() => {
-  //   setHomeSelected(false)
-  // })
 
   return (
-    <Grid container className={classes.bar}>
-      <Grid item xs={3} onClick={handleHomeSelect}>
-        <Typography className={classes.logo}>
-          synesthete
-        </Typography>
+    <div>
+      <Grid container className={classes.bar}>
+        <Grid item xs={3} onClick={handleHomeSelect}>
+          <Typography className={classes.logo}>
+            synesthete
+          </Typography>
+        </Grid>
+        <Grid item xs={2} className={classes.button} onClick={handleHomeSelect}>
+          <Typography className={classes.textNotCurrentPage}>
+            home
+          </Typography>
+        </Grid>
+        <Grid item xs={2} className={classes.button} onClick={handleSearchSelect}>
+          <Typography className={classes.textNotCurrentPage}>
+            search
+          </Typography>
+        </Grid>
+        <Grid item xs={2} className={classes.button} onClick={handleUploadSelect}>
+          <Typography className={classes.textNotCurrentPage}>
+            upload
+          </Typography>
+        </Grid>
+        <Grid item xs={2} className={classes.button} onClick={handleAboutSelect}>
+          <Typography className={classes.textNotCurrentPage}>
+            about
+          </Typography>
+        </Grid>
       </Grid>
-      <Grid item xs={2} className={classes.button} onClick={handleHomeSelect}>
-        <Typography className={classes.textNotCurrentPage}>
-          home
-        </Typography>
-      </Grid>
-      <Grid item xs={2} className={classes.button}>
-        <Typography className={classes.textNotCurrentPage}>
-          search
-        </Typography>
-      </Grid>
-      <Grid item xs={2} className={classes.button}>
-        <Typography className={classes.textNotCurrentPage}>
-          upload
-        </Typography>
-      </Grid>
-      <Grid item xs={2} className={classes.button}>
-        <Typography className={classes.textNotCurrentPage}>
-          about
-        </Typography>
-      </Grid>
-    </Grid>
+      {homeSelected &&
+        <Redirect to={'/'} />
+      }
+      {searchSelected &&
+        <Redirect to={'/search'} />
+      }
+      {uploadSelected &&
+        <Redirect to={'/upload'} />
+      }
+      {aboutSelected &&
+        <Redirect to={'/about'} />
+      }
+    </div>
   )
 }
 
