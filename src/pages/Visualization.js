458b5cddbb2bf040211840d98559a9c477eb3d91
Lucille Njoo <lucillenjoo@gmail.com>
2019-10-26 16:00:32 -0700

    Major cleanup and refactor of loadMusic in Visualization

--- src/pages/Visualization.js
+++ src/pages/Visualization.js
@@ -132,8 +132,7 @@ export const loadMusic = (
   existingAudioNode,
   setAudioNode,
   existingAnalyser,
-  setAnalyser,
-
+  setAnalyser
 ) => {
   console.log("CALLING SETUPANDPLAYANIMATION!!")
 
@@ -141,31 +140,22 @@ export const loadMusic = (
 
   audio.src = srcUrl
 
-  const canvas = canvasRef.current;
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  const ctx = canvas.getContext("2d");
+  const canvas = canvasRef.current
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  const ctx = canvas.getContext("2d")
 
   const context = (existingContext) ? existingContext : new AudioContext()
   const audioNode = (existingAudioNode) ? existingAudioNode : context.createMediaElementSource(audio)
   const analyser = (existingAnalyser) ? existingAnalyser : context.createAnalyser()
 
-  audioNode.connect(analyser); // Connects the audio context source to the analyser
+  audioNode.connect(analyser)
 
-  analyser.connect(context.destination); // End destination of an audio graph in a given context
-  analyser.fftSize = 16384;
+  analyser.connect(context.destination) // End destination of an audio graph in a given context
+  analyser.fftSize = 16384
 
-  const bufferLength = analyser.frequencyBinCount;
-  const dataArray = new Uint8Array(bufferLength);
-
-  // const WIDTH = canvas.width;
-  // const HEIGHT = canvas.height;
-  //
-  // const barWidth = (WIDTH / bufferLength) * 40;
-  //
-  // let barHeight;
-  // let x = 0;
-
+  const bufferLength = analyser.frequencyBinCount
+  const dataArray = new Uint8Array(bufferLength)
 
   const colors = [
     [179, 0, 54], // red
@@ -183,8 +173,8 @@ export const loadMusic = (
     [145, 10, 145], // magenta
   ]
 
-  function getColor(index, offset) {
-    let newIndex = index + offset
+  function getColorOfSoundBars(keyIndex, offset) {
+    let newIndex = keyIndex + offset
     if (newIndex >= colors.length) {
       newIndex -= colors.length
     }
@@ -196,76 +186,44 @@ export const loadMusic = (
 
   const WIDTH = canvas.width;
   const HEIGHT = canvas.height;
-
-  const barWidth = (WIDTH / bufferLength) * 40;
+  const totalNumberOfBars = 120
+  const barWidth = (WIDTH / bufferLength) * 40
+  const barHeightMax = 500;
+  const barSpacing = 10
+  const barsPerColorSection = 6
 
   let barHeight;
   let x = 0;
 
-  function renderFrame(/*{ canvas, analyser, dataArray, ctx, key = 'C', acousticness = 0.5, energy = 0.5 }*/) {
+  function renderFrame() {
+    requestAnimationFrame(renderFrame)
 
     x = 0;
 
-    // console.log("RENDERFRAME CALLED")
-    requestAnimationFrame(renderFrame); // Takes callback function to invoke before rendering
+    analyser.getByteFrequencyData(dataArray)
 
-    analyser.getByteFrequencyData(dataArray);
+    // Clears canvas with black and use opacity 0.1 to create fade effect
+    ctx.fillStyle = "rgba(0,0,0,0.1)"
+    ctx.fillRect(0, 0, WIDTH, HEIGHT)
 
-    ctx.fillStyle = "rgba(0,0,0,0.1)"; // Clears canvas before rendering bars (black with opacity 0.2)
-    ctx.fillRect(0, 0, WIDTH, HEIGHT); // Fade effect, set opacity to 1 for sharper rendering of bars
+    for (let barIndex = 0; barIndex < totalNumberOfBars; barIndex++) {
+      barHeight = (dataArray[barIndex] * 2);
 
-    let r, g, b, a;
-    let bars = 120 // Set total number of bars you want per frame
-
-
-    for (let i = 0; i < bars; i++) {
-      barHeight = (dataArray[i] * 2);
-      const barHeightMax = 255 * 2;
-      const multiple = 6
       // offset = -10 // Lord Huron, the Night We Met (key: A major)
       let offset = -7 // Kina Grannis, Iris (key: F# Major)
 
-      if (i < 1 * multiple){ // red
-        [r, g, b] = getColor(0, offset)
-      } else if (i < 2 * multiple){ // red-orange
-        [r, g, b] = getColor(1, offset)
-      } else if (i < 3 * multiple){ // orange
-        [r, g, b] = getColor(2, offset)
-      } else if (i < 4 * multiple){ // yellow
-        [r, g, b] = getColor(3, offset)
-      } else if (i < 5 * multiple){ // light yellow
-        [r, g, b] = getColor(4, offset)
-      } else if (i < 6 * multiple){ // yellow-green
-        [r, g, b] = getColor(5, offset)
-      } else if (i < 7 * multiple){ // green
-        [r, g, b] = getColor(6, offset)
-      } else if (i < 8 * multiple){ // blue-green
-        [r, g, b] = getColor(7, offset)
-      } else if (i < 9 * multiple){ // green-blue
-        [r, g, b] = getColor(8, offset)
-      } else if (i < 10 * multiple){ // dark blue
-        [r, g, b] = getColor(9, offset)
-      } else if (i < 11 * multiple){ // indigo
-        [r, g, b] = getColor(10, offset)
-      } else if (i < 12 * multiple){ // dark purple
-        [r, g, b] = getColor(11, offset)
-      } else { // magenta
-        [r, g, b] = getColor(12, offset)
-      }
-
-      a = (barHeight / barHeightMax) ** 3.5;
+      let [r, g, b] = getColorOfSoundBars(Math.floor(barIndex/barsPerColorSection), offset)
+      let a = (barHeight / barHeightMax) ** 3.5;
 
       ctx.fillStyle = `rgba(${r},${g},${b},${a})`;
       ctx.fillRect(x, (HEIGHT - barHeight), barWidth, barHeight);
 
-      x += barWidth + 10 // Gives 10px space between each bar
+      x += barWidth + barSpacing
     }
   }
 
-
-  audio.play();
-  renderFrame();
-  // setUpAndPlayAnimation(canvas, analyser, bufferLength, dataArray, ctx);
+  audio.play()
+  renderFrame()
 
   if (!existingAudioNode) {
     setAudioNode(audioNode)
@@ -392,153 +350,6 @@ const Visualization = props => {
       setError(defaultErrorText)
     }
   }
-
-  /*-------------------------------------------
-                LOAD MUSIC FILE
-  -------------------------------------------*/
-  // const loadMusicFile = () => {
-  //   console.log("CALLING loadMusicFile!!")
-  //
-  //   const audio = audioRef.current
-  //
-  //   audio.src = trackObject.preview_url
-  //
-  //   const canvas = canvasRef.current;
-  //   canvas.width = window.innerWidth;
-  //   canvas.height = window.innerHeight;
-  //   const ctx = canvas.getContext("2d");
-  //
-  //   const context = (existingContext) ? existingContext : new AudioContext()
-  //   const audioNode = (existingAudioNode) ? existingAudioNode : context.createMediaElementSource(audio)
-  //   const analyser = (existingAnalyser) ? existingAnalyser : context.createAnalyser()
-  //
-  //   audioNode.connect(analyser); // Connects the audio context source to the analyser
-  //
-  //   analyser.connect(context.destination); // End destination of an audio graph in a given context
-  //   analyser.fftSize = 16384;
-  //
-  //   const bufferLength = analyser.frequencyBinCount;
-  //   const dataArray = new Uint8Array(bufferLength);
-  //
-  //   // const WIDTH = canvas.width;
-  //   // const HEIGHT = canvas.height;
-  //   //
-  //   // const barWidth = (WIDTH / bufferLength) * 40;
-  //   //
-  //   // let barHeight;
-  //   // let x = 0;
-  //
-  //
-  //   const colors = [
-  //     [179, 0, 54], // red
-  //     [255, 60, 0], // red-orange
-  //     [255, 123, 0], // orange
-  //     [255, 178, 0], // honey yellow
-  //     [255, 243, 163], // light yellow
-  //     [203, 247, 57], // yellow-green
-  //     [25, 191, 0], // green
-  //     [0, 161, 155], // blue-green
-  //     [0, 181, 169], // green-blue
-  //     [0, 113, 212], // blue
-  //     [39, 16, 173], // indigo
-  //     [75, 0, 145], // dark purple
-  //     [145, 10, 145], // magenta
-  //   ]
-  //
-  //   function getColor(index, offset) {
-  //     let newIndex = index + offset
-  //     if (newIndex >= colors.length) {
-  //       newIndex -= colors.length
-  //     }
-  //     if (newIndex < 0) {
-  //       newIndex += colors.length
-  //     }
-  //     return colors[newIndex]
-  //   }
-  //
-  //   const WIDTH = canvas.width;
-  //   const HEIGHT = canvas.height;
-  //
-  //   const barWidth = (WIDTH / bufferLength) * 40;
-  //
-  //   let barHeight;
-  //   let x = 0;
-  //
-  //   function renderFrame(/*{ canvas, analyser, dataArray, ctx, key = 'C', acousticness = 0.5, energy = 0.5 }*/) {
-  //
-  //     x = 0;
-  //
-  //     // console.log("RENDERFRAME CALLED")
-  //     requestAnimationFrame(renderFrame); // Takes callback function to invoke before rendering
-  //
-  //     analyser.getByteFrequencyData(dataArray);
-  //
-  //     ctx.fillStyle = "rgba(0,0,0,0.1)"; // Clears canvas before rendering bars (black with opacity 0.2)
-  //     ctx.fillRect(0, 0, WIDTH, HEIGHT); // Fade effect, set opacity to 1 for sharper rendering of bars
-  //
-  //     let r, g, b, a;
-  //     let bars = 120 // Set total number of bars you want per frame
-  //
-  //
-  //     for (let i = 0; i < bars; i++) {
-  //       barHeight = (dataArray[i] * 2);
-  //       const barHeightMax = 255 * 2;
-  //       const multiple = 6
-  //       // offset = -10 // Lord Huron, the Night We Met (key: A major)
-  //       let offset = -7 // Kina Grannis, Iris (key: F# Major)
-  //
-  //       if (i < 1 * multiple){ // red
-  //         [r, g, b] = getColor(0, offset)
-  //       } else if (i < 2 * multiple){ // red-orange
-  //         [r, g, b] = getColor(1, offset)
-  //       } else if (i < 3 * multiple){ // orange
-  //         [r, g, b] = getColor(2, offset)
-  //       } else if (i < 4 * multiple){ // yellow
-  //         [r, g, b] = getColor(3, offset)
-  //       } else if (i < 5 * multiple){ // light yellow
-  //         [r, g, b] = getColor(4, offset)
-  //       } else if (i < 6 * multiple){ // yellow-green
-  //         [r, g, b] = getColor(5, offset)
-  //       } else if (i < 7 * multiple){ // green
-  //         [r, g, b] = getColor(6, offset)
-  //       } else if (i < 8 * multiple){ // blue-green
-  //         [r, g, b] = getColor(7, offset)
-  //       } else if (i < 9 * multiple){ // green-blue
-  //         [r, g, b] = getColor(8, offset)
-  //       } else if (i < 10 * multiple){ // dark blue
-  //         [r, g, b] = getColor(9, offset)
-  //       } else if (i < 11 * multiple){ // indigo
-  //         [r, g, b] = getColor(10, offset)
-  //       } else if (i < 12 * multiple){ // dark purple
-  //         [r, g, b] = getColor(11, offset)
-  //       } else { // magenta
-  //         [r, g, b] = getColor(12, offset)
-  //       }
-  //
-  //       a = (barHeight / barHeightMax) ** 3.5;
-  //
-  //       ctx.fillStyle = `rgba(${r},${g},${b},${a})`;
-  //       ctx.fillRect(x, (HEIGHT - barHeight), barWidth, barHeight);
-  //
-  //       x += barWidth + 10 // Gives 10px space between each bar
-  //     }
-  //   }
-  //
-  //
-  //   audio.play();
-  //   renderFrame();
-  //   // setUpAndPlayAnimation(canvas, analyser, bufferLength, dataArray, ctx);
-  //
-  //   if (!existingAudioNode) {
-  //     setAudioNode(audioNode)
-  //   }
-  //   if (!existingAnalyser) {
-  //     setAnalyser(analyser)
-  //   }
-  //   if (!existingContext) {
-  //     setContext(context)
-  //   }
-  // }
 
   return (
     <div className={classes.root}>
