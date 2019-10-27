600c003e4b9e05ebbda2af73e04c044b32cfaef8
Lucille Njoo <lucillenjoo@gmail.com>
2019-10-27 13:33:45 -0700

    Move webfont to index.html rather than App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import { withStyles } from '@material-ui/core/styles'
@@ -21,9 +21,7 @@ export const theme = createMuiTheme({
     },
     type: 'dark',
     dark: {
-      // pink: '#c23063',
       pink: '#bf265c',
-      // purple: '#10002e',
       purple: '#1c004f',
     },
     medium: {
@@ -43,10 +41,8 @@ export const theme = createMuiTheme({
 const styles = () => ({
   root: {
     width: '100vw',
-    // height: '100vh',
     height: '100%',
     minHeight: '100vh',
-    // background: `linear-gradient(0deg, ${theme.palette.dark.pink} -25%, ${theme.palette.dark.purple} 80%)`
     background: `linear-gradient(0deg, ${theme.palette.dark.pink} 0%, ${theme.palette.dark.purple} 80%)`
   }
 })
@@ -54,9 +50,6 @@ const styles = () => ({
 const routes = (
   <Router>
     <div>
-      {/* <Switch>
-        <Route exact path="*" component={MenuBar}/>
-      <Switch> */}
       <MenuBar />
       <Switch>
         <Route exact path="/" component={Homepage} />
@@ -68,23 +61,16 @@ const routes = (
       </Switch>
     </div>
   </Router>
-);
-// TODO: Add a Page Not Found
-// <Route exact path="*" component={NotFound} />
-
+)
 
 const App = (props) => {
-  // Because App is the "uppermost" component (see index.js), code in the useEffect function
-  // is equivalent to an overall initialization routine. Note however that every component
-  // can have its own useEffect, and so initialization can be separated on a per-component
-  // basis.
-  useEffect(() => apiHost('http://api.giphy.com/v1/'))
+
+  useEffect(() => apiHost('https://api.spotify.com/v1/'))
 
   const { classes } = props
 
   return (
     <div className={classes.root}>
-      <style>@import url('https://fonts.googleapis.com/css?family=Dosis&display=swap');</style>
       <MuiThemeProvider theme={theme}>
         {routes}
       </MuiThemeProvider>
