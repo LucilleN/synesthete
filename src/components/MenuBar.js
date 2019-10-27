430b716cb25625a149b27305e4177f9de6e2f4a0
Lucille Njoo <lucillenjoo@gmail.com>
2019-10-27 16:13:24 -0700

    Fix all warnings

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
 
-import { Redirect, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const styles = theme => ({
   bar: {
