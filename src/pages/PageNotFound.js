d8a78635f64865974ce093122192f1bad2b25103
Lucille Njoo <lucillenjoo@gmail.com>
2019-10-27 21:21:28 -0700

    Create PageNotFound and tests

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+
+import StartButton from '../components/StartButton'
+
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    color: theme.palette.light.pink,
+    fontSize: '5rem',
+    textAlign: 'center',
+    marginTop: '30vh',
+    opacity: 0.8
+  }
+})
+
+const PageNotFound = props => {
+  const { classes } = props
+  return (
+    <div className={classes.root}>
+      <Typography className={classes.title} id="title">
+        page not found
+      </Typography>
+      <StartButton href={"/"} text={"back to home"} id="back-to-home" />
+    </div>
+  )
+}
+
+export default withStyles(styles)(PageNotFound)
---
+++ src/pages/PageNotFound.test.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TestRenderer from 'react-test-renderer'
+import ReactTestUtils from 'react-dom/test-utils'
+
+import { MuiThemeProvider } from '@material-ui/core/styles'
+
+import { theme } from '../App'
+import PageNotFound from './PageNotFound'
+
+let component
+beforeEach(() => {
+  component = TestRenderer.create(
+    <MuiThemeProvider theme={theme}>
+      <PageNotFound />
+    </MuiThemeProvider>
+  )
+})
+
+describe('initial state', () => {
+  let div
+  beforeEach(() => {
+    div = document.createElement('div')
+    ReactTestUtils.act(() => {
+      ReactDOM.render(
+        <MuiThemeProvider theme={theme}>
+          <PageNotFound />
+        </MuiThemeProvider>
+      , div)
+    })
+  })
+
+  it('should have the title "page not found"', () => {
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+    const pageTitle = div.querySelector('#title')
+    expect(pageTitle.textContent).toBe('page not found')
+  })
+
+  it('should have a button that redirects to home', () => {
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+    const backToHome = div.querySelector('#back-to-home')
+    expect(backToHome).not.toBeNull()
+  })
+})
+
+describe('when the "back to home" button is clicked', () => {
+  let div
+  beforeEach(() => {
+    div = document.createElement('div')
+    ReactTestUtils.act(() => {
+      ReactDOM.render(
+        <MuiThemeProvider theme={theme}>
+          <PageNotFound />
+        </MuiThemeProvider>
+      , div)
+    })
+    const button = div.querySelector('button')
+    ReactTestUtils.act(() => {
+      ReactTestUtils.Simulate.click(button)
+    })
+  })
+
+  it('should redirect from the page', () => {
+    const pageTitle = div.querySelector('#title')
+    expect(pageTitle).toBeNull()
+  })
+})
